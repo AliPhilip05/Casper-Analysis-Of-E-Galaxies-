2ee779617eac0c9a978bf1327f4a031072f21e01
Ali Philip <131696518+AliPhilip05@users.noreply.github.com>
2026-05-30 20:50:04 -0400

    Update query.py

--- query.py
+++ query.py
@@ -11,7 +11,27 @@ from lsst.rsp import get_query_history
 hist=await get_query_history()
 hist
 
-my_adql_query = { "SELECT TOP 100000 coord_ra, coord_dec FROM dp02_dc2_catalogs.Object" } 
+my_adql_query = { "SELECT TOP 10000
+    objectId,
+    coord_ra,
+    coord_dec,
+
+    g_cModelFlux,
+    r_cModelFlux,
+    i_cModelFlux,
+
+    g_kronRad,
+    r_kronRad,
+    i_kronRad,
+
+    shape_xx,
+    shape_xy,
+    shape_yy,
+
+    refExtendedness
+
+FROM dp02_dc2_catalogs.Object
+WHERE detect_isPrimary = 1" } 
 service = get_tap_service("tap")
 results = service.search(my_adql_query)
 results_table = results.to_table()
@@ -29,5 +49,15 @@ ORDER BY column_name
 service = get_tap_service("tap")
 results = service.search(my_adql_query)
 results_table = results.to_table()
+
+import os
+
+print(os.getcwd())
+
+results_table.write(
+    "dc2_objects.txt",
+    format="ascii.tab",
+    overwrite=True
+)
 
 print(results_table)
